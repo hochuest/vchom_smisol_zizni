322b002862fa818676a863a1cc5eca8bbb0ee2c4
theLoopa <istatenin@bk.ru>
2018-07-02 03:09:11 +0400

    main

--- testwpf/MainWindow.xaml.cs
+++ testwpf/MainWindow.xaml.cs
@@ -1,20 +1,7 @@
 ﻿using System;
 using System.Windows.Forms; // !!!!! Лиза, wpf 
-using System.ComponentModel;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using MahApps.Metro.Controls;
 using System.Collections.ObjectModel; // для ObservableCollection
 
 using testwpf.whiskas;
